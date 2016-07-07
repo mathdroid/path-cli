c83ad4bb2fa29a2f83bb24b271b1ec98b29802fc
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2016-07-07 19:02:28 +0700

    early inquirer version

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 const request = require('request')
 const moment = require('moment')
+const inquirer = require('inquirer')
+
 const config = require('./config')
 
 let jsonRes = {}
+let momentsArray = [] //metadata for moments
+let tlArray = [] // displayed on tl
+
+let showTimeline = (tla) => {
+  // tla.forEach((value) => {
+  //   console.log(value)
+  // })
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'theme',
+      message: 'What do you want to do?',
+      choices: tla
+    }
+  ]).then(function (answers) {
+    console.log(JSON.stringify(answers, null, '  '));
+  })
+
+}
 
 let createTimeline = (tljson) => {
-  tljson.moments.
-    map((obj) => obj.type + ': ' + (typeof(obj.comments[0])==='object' ? obj.comments[0].body : obj.headline) + '\n(' + obj.seen_its.total + ' views), ' + moment.unix(obj.created).fromNow() +'\nBy: ' + tljson.users[obj.user_id].first_name + ' ' + tljson.users[obj.user_id].last_name + '\n' ).
-    forEach((value) => {console.log(value)})
+  momentsArray = tljson.moments
+  return momentsArray.
+    map((obj) => tljson.users[obj.user_id].first_name + ' ' + tljson.users[obj.user_id].last_name + '\'s ' + obj.type + ': ' + (typeof(obj.comments[0])==='object' ? obj.comments[0].body : obj.headline) + ' (' + obj.seen_its.total + ' views), ' + moment.unix(obj.created).fromNow() +'' )
+
 }
 
 request('https://api.path.com/3/moment/feed/home?oauth_token='+config.USER_TOKEN+'&limit=60&user_id=4f71d93d3b954a3e53001857&gs=1', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     jsonRes = JSON.parse(body)
-    createTimeline(jsonRes)
+    showTimeline(createTimeline(jsonRes))
     // console.dir(jsonRes.moments);
   } else {
     console.error(response)
