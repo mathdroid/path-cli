c630ed0aae542d9ba560702dd40350c21b30e660
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2016-07-07 19:45:58 +0700

    download photo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const request = require('request')
 const moment = require('moment')
 const inquirer = require('inquirer')
+const chalk = require('chalk')
+const fs = require('fs')
 
 const config = require('./config')
 
@@ -8,19 +10,26 @@ let jsonRes = {}
 let momentsArray = [] //metadata for moments
 let tlArray = [] // displayed on tl
 
+
 let showTimeline = (tla) => {
-  // tla.forEach((value) => {
-  //   console.log(value)
-  // })
   inquirer.prompt([
     {
       type: 'list',
-      name: 'theme',
-      message: 'What do you want to do?',
+      name: 'moments',
+      message: 'Your timeline:',
       choices: tla
     }
   ]).then(function (answers) {
-    console.log(JSON.stringify(answers, null, '  '));
+    let mo = momentsArray[tla.indexOf(answers.moments)]
+    console.log(mo.type)
+    mo.type=='photo' ? console.log(mo.photo.photo.url+'/'+mo.photo.photo.square.file) : console.log(mo)
+    console.log('\n\nDownloading photo...\n\n')
+    request
+      .get(mo.photo.photo.url+'/'+mo.photo.photo.square.file)
+      .on('error', function(err) {
+        console.log(err)
+      })
+      .pipe(fs.createWriteStream('photo.jpg'))
   })
 
 }
@@ -28,10 +37,10 @@ let showTimeline = (tla) => {
 let createTimeline = (tljson) => {
   momentsArray = tljson.moments
   return momentsArray.
-    map((obj) => tljson.users[obj.user_id].first_name + ' ' + tljson.users[obj.user_id].last_name + '\'s ' + obj.type + ': ' + (typeof(obj.comments[0])==='object' ? obj.comments[0].body : obj.headline) + ' (' + obj.seen_its.total + ' views), ' + moment.unix(obj.created).fromNow() +'' )
+    map((obj) => (chalk.bold.white(tljson.users[obj.user_id].first_name) + ' ' + tljson.users[obj.user_id].last_name) + '\'s ' + chalk.white.underline(obj.type) + ': ' + (typeof(obj.comments[0])==='object' ? obj.comments[0].body : obj.headline) + chalk.magenta(' (' + obj.seen_its.total + ' views)') + ', ' + chalk.grey(moment.unix(obj.created).fromNow()) +'' )
 
 }
-
+console.log(chalk.bold('Please wait while we initialize your ' + chalk.bgRed('Path') + ' timeline.'))
 request('https://api.path.com/3/moment/feed/home?oauth_token='+config.USER_TOKEN+'&limit=60&user_id=4f71d93d3b954a3e53001857&gs=1', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     jsonRes = JSON.parse(body)
