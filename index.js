227c5554af0a43e89bcdfc4b9997164d50989cef
Sonny Lazuardi <sonnylazuardi@gmail.com>
2016-07-09 21:58:33 +0700

    add line space for timeline list

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ let showTimeline = (tla) => {
 let createTimeline = (tljson) => {
   momentsArray = tljson.moments
   tlArray = momentsArray.
-    map((obj) => (chalk.bold.white(tljson.users[obj.user_id].first_name) + ' ' + tljson.users[obj.user_id].last_name) + '\'s ' + chalk.white.underline(obj.type) + ': ' + (typeof(obj.comments[0])==='object' ? (obj.comments[0].body).split('\n').join('⏎ ') : obj.headline) + chalk.magenta(' (' + obj.seen_its.total + ' views)') + ', ' + chalk.grey(moment.unix(obj.created).fromNow()) +'' )
+    map((obj) => (chalk.bold.white(tljson.users[obj.user_id].first_name) + ' ' + tljson.users[obj.user_id].last_name) + '\'s ' + chalk.white.underline(obj.type) + ': ' + (typeof(obj.comments[0])==='object' ? (obj.comments[0].body).split('\n').join('⏎ ') : obj.headline) + chalk.magenta(' (' + obj.seen_its.total + ' views)') + ', ' + chalk.grey(moment.unix(obj.created).fromNow()) +'\n\n' )
   return tlArray
 }
 
