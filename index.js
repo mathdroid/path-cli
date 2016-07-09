1079a4e3ea5649fd24eccfc3030307fbae802d67
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2016-07-09 10:02:45 +0700

    formatting

--- index.js
+++ index.js
@@ -8,20 +8,13 @@ const fs = require('fs')
 const imageToAscii = require("image-to-ascii")
 
 // const config = require('./config')
-
 const utoken = process.env.PATH_CLI_USER_TOKEN || null
-
-
-if (utoken==null) {
-  console.error('add user token in PATH_CLI_USER_TOKEN.')
-} else {
 
 let tlurl = 'https://api.path.com/3/moment/feed/home?oauth_token=' + utoken + '&limit=60&user_id=4f71d93d3b954a3e53001857&gs=1'
 
 let jsonRes = {}
 let momentsArray = [] //metadata for moments
 let tlArray = [] // displayed on tl
-
 
 let showTimeline = (tla) => {
   tla = tla.concat([
@@ -87,15 +80,19 @@ let createTimeline = (tljson) => {
     map((obj) => (chalk.bold.white(tljson.users[obj.user_id].first_name) + ' ' + tljson.users[obj.user_id].last_name) + '\'s ' + chalk.white.underline(obj.type) + ': ' + (typeof(obj.comments[0])==='object' ? obj.comments[0].body : obj.headline) + chalk.magenta(' (' + obj.seen_its.total + ' views)') + ', ' + chalk.grey(moment.unix(obj.created).fromNow()) +'' )
   return tlArray
 }
-console.log(chalk.bold('Please wait while we initialize your ' + chalk.bgRed('Path') + ' timeline.'))
 
-request(tlurl, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    jsonRes = JSON.parse(body)
-    showTimeline(createTimeline(jsonRes))
-    // console.dir(jsonRes.moments);
-  } else {
-    console.error(response)
-  }
-})
+if (utoken==null) {
+  console.error('add user token in PATH_CLI_USER_TOKEN.')
+} else {
+  console.log(chalk.bold('Please wait while we initialize your ' + chalk.bgRed('Path') + ' timeline.'))
+
+  request(tlurl, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      jsonRes = JSON.parse(body)
+      showTimeline(createTimeline(jsonRes))
+      // console.dir(jsonRes.moments);
+    } else {
+      console.error(response)
+    }
+  })
 }
