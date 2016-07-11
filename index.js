792c60935d11b1da3dfd23a805efadac9c1bc770
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2016-07-11 15:44:56 +0700

    dns lookup for connectivity

--- index.js
+++ index.js
@@ -7,11 +7,19 @@ const Login = require('./ui/views/LoginView')
 const Timeline = require('./ui/views/TimelineView')
 const helper = require('./lib/helper')
 
-const utoken = helper.getConfig().oauth_token || null
 
-if (utoken == null) {
-  console.log(chalk.red('✗') + ' OAuth token not found. Please login with your ' + chalk.bgRed.bold('Path') + ' credentials.')
-  Login.login(logger)
-} else {
-  logger(utoken)
+let logger = (msg) => {
+  console.log(msg)
 }
+
+let main = () => {
+  const utoken = helper.getConfig().oauth_token || null
+  if (utoken == null) {
+    console.log(chalk.red('✗') + ' OAuth token not found. Please login with your ' + chalk.bgRed.bold('Path') + ' credentials.')
+    Login.login(logger)
+  } else {
+    logger(utoken)
+  }
+}
+
+helper.getConnection(main)
